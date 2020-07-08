039bc59457e42464f9865e7af59f2c91661d731f
Kevin Anaro-Wood <k.a.wood91@gmail.com>
2020-07-08 15:51:22 -0400

    Partially working
    Vid loads but src of second video is blocked due to content settings

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -30,7 +30,10 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-})
+
+
+
+
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -39,5 +42,5 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+})
---
+++ test.js
@@ -0,0 +1,34 @@
+document.getElementById("myButton").addEventListener("click", myFunction);
+
+// function myFunction(){
+//   console.log('asd');
+// }
+
+var vid = document.getElementById("myVideo");
+var statusElement = document.getElementById("status");
+var currentlyPlaying = 1;
+var currentlPlayingTime;
+
+var src1 = "C:\Users\rocke\Downloads\oscar.mp4";
+
+var src2 = "https://www.youtube.com/watch?v=TnXz_nnQZrw&t=186s";
+
+document.getElementById("myButton").addEventListener("click", console.log("test"));
+
+function myFunction() {
+  currentlPlayingTime = vid.currentTime;
+  if (currentlyPlaying === 1) {
+      vid.src = src2;
+      currentlyPlaying = 2;
+      statusElement.innerText = 'Going to play video2..';
+  } else {
+      vid.src = src1;
+      currentlyPlaying = 1;
+      statusElement.innerText = 'Going to play video1..';
+  }
+  vid.load();
+  vid.addEventListener('loadedmetadata', function () {
+      vid.currentTime = currentlPlayingTime;
+  }, false);
+//   console.log('asd');
+}
