2a326c67c9d928b789ea87c2b9e454de430f74d5
Kevin Anaro-Wood <k.a.wood91@gmail.com>
2020-07-09 19:40:58 -0400

    Bootstrap and GSAP aded
    Theme is now built with Bootstrap. Working on getting GSAP animations to work

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 document.getElementById("myButton").addEventListener("click", myFunction);
 document.getElementById("mybody").addEventListener("keypress", myFunction2);
 
+(function(){
+    document.addEventListener('DOMContentLoaded', init, false);
+    frameTwo();
+
+    });
 // function myFunction(){
 //   console.log('asd');
 // }
@@ -10,11 +15,9 @@ var statusElement = document.getElementById("status");
 var currentlyPlaying = 1;
 var currentlPlayingTime;
 
-var src1 = "C:\\Users\\rocke\\Downloads\\oscar.mp4";
-
-var src2 = "C:\\Users\\rocke\\Downloads\\oscar2.mp4";
-
-var src3 = "C:\\Users\\rocke\\Downloads\\oscar2.mp4";
+var src1 = "https://www.youtube.com/embed/7XhYS3WFt58?controls=0";
+var src2 = "https://www.youtube.com/embed/_yhtJLlvnwE";
+var src3 = "https://www.youtube.com/embed/R2pIutTspQA";
 
 
 document.getElementById("myButton").addEventListener("click", console.log("test"));
@@ -24,13 +27,13 @@ function myFunction() {
   if (currentlyPlaying === 1) {
       vid.src = src2;
       currentlyPlaying = 2;
-      statusElement.innerText = 'Going to play video2..';
+      statusElement.innerText = 'Going to play video #2..';
   } else {
       vid.src = src1;
       currentlyPlaying = 1;
-      statusElement.innerText = 'Going to play video1..';
+      statusElement.innerText = 'Going to play video #1..';
   }
-  vid.load();
+//   vid.load();
   vid.addEventListener('loadedmetadata', function () {
       vid.currentTime = currentlPlayingTime;
   }, false);
@@ -39,16 +42,35 @@ function myFunction() {
 function myFunction2() {
     currentlPlayingTime = vid.currentTime;
     if (currentlyPlaying === 1) {
-        vid.src = src2;
+        vid.src = src3;
         currentlyPlaying = 3;
-        statusElement.innerText = 'Going to play video3..';
-    } else {
+        statusElement.innerText = 'Going to play video #3..';
+    } 
+    else if (currentlyPlaying === 3){
         vid.src = src1;
         currentlyPlaying = 1;
-        statusElement.innerText = 'Going to play video1..';
+        statusElement.innerText = 'Going to paly video #1'
     }
-    vid.load();
+    else {
+        vid.src = src2;
+        currentlyPlaying = 1;
+        statusElement.innerText = 'Going to play video #1..';
+    }
+    // vid.load();
     vid.addEventListener('loadedmetadata', function () {
         vid.currentTime = currentlPlayingTime;
     }, false);
+  }
+  TweenMax.to(".myvideo", {rotation: 27, x: 100, duration: 1});
+
+  function frameTwo() {
+    TweenMax.set([
+        myVideo,
+      // glow2_1
+    ], {opacity:1});
+    TweenMax.set([myVideo], {opacity: 0}); 
+
+    var tl = new TimelineMax({});
+    tl.from([myVideo],  0.3, {x:70, opacity:0}, "+=0.4");
+
   }
