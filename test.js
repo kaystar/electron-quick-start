d39179e0bbd6a0ece66cc7b17e70bc0125d781fc
Kevin Anaro-Wood <k.a.wood91@gmail.com>
2020-07-10 12:17:51 -0400

    Greensock aniamtions working

--- test.js
+++ test.js
@@ -1,14 +1,6 @@
 document.getElementById("mybody").addEventListener("click", myFunction);
 document.getElementById("mybody").addEventListener("keypress", myFunction2);
 
-(function(){
-    document.addEventListener('DOMContentLoaded', init, false);
-    frameTwo();
-
-    });
-// function myFunction(){
-//   console.log('asd');
-// }
 
 var vid = document.getElementById("myVideo");
 var statusElement = document.getElementById("demo");
@@ -17,10 +9,9 @@ var currentlPlayingTime;
 
 var src1 = "https://www.youtube.com/embed/7XhYS3WFt58?controls=0&autoplay=1&mute=0";
 var src2 = "https://www.youtube.com/embed/_yhtJLlvnwE?autoplay=1&mute=0";
-var src3 = "https://www.youtube.com/embed/R2pIutTspQA?autoplay=1&mute=0";
+var src3 = "https://www.youtube.com/embed/y3RIHnK0_NE?autoplay=1&mute=0";
 
-
-document.getElementById("myButton").addEventListener("click", console.log("test"));
+// document.getElementById("myButton").addEventListener("click", console.log("test"));
 
 function myFunction() {
   currentlPlayingTime = vid.currentTime;
@@ -28,10 +19,12 @@ function myFunction() {
       vid.src = src2;
       currentlyPlaying = 2;
       statusElement.innerText = 'Going to play video #2..';
+      animate()
   } else {
       vid.src = src1;
       currentlyPlaying = 1;
       statusElement.innerText = 'Going to play video #1..';
+      animate()
   }
 //   vid.load();
   vid.addEventListener('loadedmetadata', function () {
@@ -45,32 +38,31 @@ function myFunction2() {
         vid.src = src3;
         currentlyPlaying = 3;
         statusElement.innerText = 'Going to play video #3..';
+        animate()
     } 
     else if (currentlyPlaying === 3){
         vid.src = src1;
         currentlyPlaying = 1;
         statusElement.innerText = 'Going to play video #1'
+        animate()
+        // TweenMax.to("#controls", 2, {opacity:0, x:50});
+
     }
     else {
         vid.src = src2;
         currentlyPlaying = 1;
         statusElement.innerText = 'Going to play video #1..';
+        animate()
     }
     // vid.load();
     vid.addEventListener('loadedmetadata', function () {
         vid.currentTime = currentlPlayingTime;
     }, false);
   }
-  TweenMax.to(".myvideo", {rotation: 27, x: 100, duration: 1});
 
-  function frameTwo() {
-    TweenMax.set([
-        myVideo,
-      // glow2_1
-    ], {opacity:1});
-    TweenMax.set([myVideo], {opacity: 0}); 
+  TweenMax.from("#controls", 1, {opacity:0, x:50, delay:1});
 
-    var tl = new TimelineMax({});
-    tl.from([myVideo],  0.3, {x:70, opacity:0}, "+=0.4");
+  function animate() {
+    TweenMax.from("#demo", 0.75, {opacity:0, y:-25, delay:1});
 
   }
